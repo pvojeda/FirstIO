d702fcce301a8c4b704b49ee48068c4dcf582877
Pvojeda <pojeda@mail.csuchico.edu>
2018-01-29 17:51:20 -0800

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,10 @@
+//Author: Pedro Ojeda
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is Pedro" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
